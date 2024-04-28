e53b388dafe49ea31f19314d2aa4c649f75bc4c7
Kelly <88363184+Waxomoly@users.noreply.github.com>
2024-04-28 23:58:41 +0700

    UI wo error message done

--- src/main/java/org/example/logical_calculator/HelloController.java
+++ src/main/java/org/example/logical_calculator/HelloController.java
@@ -1,11 +1,13 @@
 package org.example.logical_calculator;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 import java.util.ArrayList;
 
@@ -18,6 +20,9 @@ public class HelloController {
 
     @FXML
     private TableView myTable;
+
+    @FXML
+    private Circle tautologiLightStatus, kontradiksiLightStatus, kontingensiLightStatus;
 
     private CalculatorBody calculatorBody = new CalculatorBody();
     private LogicCalcu logicCalcu = new LogicCalcu();
@@ -89,73 +94,126 @@ public class HelloController {
     }
 
     @FXML
-    protected void enterLabel(){
-        //Clear Table
+    protected void enterLabel() {
+        // Clear Table
         myTable.getColumns().clear();
-        //set variables found status to false
+        // Set variables found status to false
         LogicCalcu.PFound = false;
         LogicCalcu.QFound = false;
-        //Count
+        // Count
         LogicCalcu.generateCombinations(calculatorBody.getEquationOnLabel());
         System.out.println("p " + LogicCalcu.PFound);
         System.out.println("q " + LogicCalcu.QFound);
-        //two conditions : if error is met (results will be empty) and if error is not met (result will show)
+        // Two conditions: if error is met (results will be empty) and if error is not met (result will show)
 
-        if(false){ //nanti ini diisi kalok ada eror
+        if (false) { // Nanti ini diisi kalok ada eror
 
-        }else{
-            //change the light (penanda hukum)
+        } else {
+            // Change the light (penanda hukum)
 
 
-            //print out the table
-            ArrayList<TableColumn> tableColumns = new ArrayList<>();
+            // Create an ObservableList to store the data items
+            ObservableList<DataItem> dataItems = FXCollections.observableArrayList();
 
-            if(LogicCalcu.PFound && LogicCalcu.QFound && (LogicCalcu.getValueArr().size()==4)){ //ada 2 variabel
-                TableColumn PCol = new TableColumn("PCol");
-                TableColumn QCol = new TableColumn("QCol");
-                TableColumn resultCol = new TableColumn("resultCol");
-                myTable.getColumns().addAll(PCol, QCol, resultCol);
-
+            // Add data items to the list based on the logic
+            if (LogicCalcu.PFound && LogicCalcu.QFound && (LogicCalcu.getValueArr().size() == 4)) { // Ada 2 variabel
                 ArrayList<Character> currAnswers = LogicCalcu.getValueArr();
 
-                myTable.getItems().add(new DataItem("0", "0", currAnswers.get(0).toString()));
-                myTable.getItems().add(new DataItem("1", "0", currAnswers.get(1).toString()));
-                myTable.getItems().add(new DataItem("0", "1", currAnswers.get(2).toString()));
-                myTable.getItems().add(new DataItem("1", "1", currAnswers.get(3).toString()));
-            }else if(LogicCalcu.getValueArr().size()==2){ //cuma ada 1 variabel (P or Q)
-                if(LogicCalcu.PFound){
-                    TableColumn PCol = new TableColumn("PCol");
-                    TableColumn resultCol = new TableColumn("resultCol");
-                    myTable.getColumns().addAll(PCol, resultCol);
-
+                dataItems.add(new DataItem("0", "0", currAnswers.get(0).toString()));
+                dataItems.add(new DataItem("1", "0", currAnswers.get(1).toString()));
+                dataItems.add(new DataItem("0", "1", currAnswers.get(2).toString()));
+                dataItems.add(new DataItem("1", "1", currAnswers.get(3).toString()));
+            } else if (LogicCalcu.getValueArr().size() == 2) { // Cuma ada 1 variabel (P or Q)
+                if (LogicCalcu.PFound) {
                     ArrayList<Character> currAnswers = LogicCalcu.getValueArr();
 
-                    myTable.getItems().add(new DataItem("0", null, currAnswers.get(0).toString()));
-                    myTable.getItems().add(new DataItem("1", null, currAnswers.get(1).toString()));
-                }else if(LogicCalcu.QFound){
-                    TableColumn QCol = new TableColumn("QCol");
-                    TableColumn resultCol = new TableColumn("resultCol");
-                    myTable.getColumns().addAll(QCol, resultCol);
-
+                    dataItems.add(new DataItem("0", null, currAnswers.get(0).toString()));
+                    dataItems.add(new DataItem("1", null, currAnswers.get(1).toString()));
+                } else if (LogicCalcu.QFound) {
                     ArrayList<Character> currAnswers = LogicCalcu.getValueArr();
 
-                    myTable.getItems().add(new DataItem(null, "0", currAnswers.get(0).toString()));
-                    myTable.getItems().add(new DataItem(null, "1", currAnswers.get(1).toString()));
-                }else{
+                    dataItems.add(new DataItem(null, "0", currAnswers.get(0).toString()));
+                    dataItems.add(new DataItem(null, "1", currAnswers.get(1).toString()));
+                } else {
                     System.out.println("Something went wrong. (HelloController.java -> enterLabel() [1])");
                 }
-            }else if(LogicCalcu.getValueArr().size()==1){ //cuma ada variabel true dan/atau false
-                TableColumn answer = new TableColumn("Single Answer");
-                myTable.getColumns().addAll(answer);
-                //add data to table
-                myTable.getItems().add(new DataItem(null, null, LogicCalcu.getValueArr().getFirst().toString()));
-            }else{
+            } else if (LogicCalcu.getValueArr().size() == 1) { // Cuma ada variabel true dan/atau false
+                dataItems.add(new DataItem(null, null, LogicCalcu.getValueArr().get(0).toString()));
+            } else {
                 System.out.println("Something went wrong. (HelloController.java -> enterLabel() [2])");
             }
 
-        }
+            // Create TableColumn objects only for the desired columns and add them to the table
+            if (LogicCalcu.PFound && LogicCalcu.QFound && (LogicCalcu.getValueArr().size() == 4)) { // Ada 2 variabel
+                TableColumn<DataItem, String> PCol = new TableColumn<>("PCol");
+                PCol.setCellValueFactory(new PropertyValueFactory<>("pValue"));
 
+                TableColumn<DataItem, String> QCol = new TableColumn<>("QCol");
+                QCol.setCellValueFactory(new PropertyValueFactory<>("qValue"));
+
+                TableColumn<DataItem, String> resultCol = new TableColumn<>("resultCol");
+                resultCol.setCellValueFactory(new PropertyValueFactory<>("resultValue"));
+
+                myTable.getColumns().addAll(PCol, QCol, resultCol);
+            } else if (LogicCalcu.getValueArr().size() == 2) { // Cuma ada 1 variabel (P or Q)
+                if (LogicCalcu.PFound) {
+                    TableColumn<DataItem, String> PCol = new TableColumn<>("PCol");
+                    PCol.setCellValueFactory(new PropertyValueFactory<>("pValue"));
+
+                    TableColumn<DataItem, String> resultCol = new TableColumn<>("resultCol");
+                    resultCol.setCellValueFactory(new PropertyValueFactory<>("resultValue"));
+
+                    myTable.getColumns().addAll(PCol, resultCol);
+                } else if (LogicCalcu.QFound) {
+                    TableColumn<DataItem, String> QCol = new TableColumn<>("QCol");
+                    QCol.setCellValueFactory(new PropertyValueFactory<>("qValue"));
+
+                    TableColumn<DataItem, String> resultCol = new TableColumn<>("resultCol");
+                    resultCol.setCellValueFactory(new PropertyValueFactory<>("resultValue"));
+
+                    myTable.getColumns().addAll(QCol, resultCol);
+                } else {
+                    System.out.println("Something went wrong. (HelloController.java -> enterLabel() [1])");
+                }
+            } else if (LogicCalcu.getValueArr().size() == 1) { // Cuma ada variabel true dan/atau false
+                TableColumn<DataItem, String> answer = new TableColumn<>("Single Answer");
+                answer.setCellValueFactory(new PropertyValueFactory<>("resultValue"));
+                myTable.getColumns().add(answer);
+            } else {
+                System.out.println("Something went wrong. (HelloController.java -> enterLabel() [2])");
+            }
+
+            // Set the ObservableList as the items list for the TableView
+            myTable.setItems(dataItems);
+
+
+            Color wrongLightStatus = Color.rgb(255, 31, 31);
+            Color rightLightStatus = Color.rgb(38, 255, 31);
+
+            //SETTINGS FOR LIGHTS
+            if(LogicCalcu.isTautologiStatus()){
+                tautologiLightStatus.setFill(rightLightStatus);
+                kontradiksiLightStatus.setFill(wrongLightStatus);
+                kontingensiLightStatus.setFill(wrongLightStatus);
+            }else if(LogicCalcu.isKontradiksiStatus()){
+                tautologiLightStatus.setFill(wrongLightStatus);
+                kontradiksiLightStatus.setFill(rightLightStatus);
+                kontingensiLightStatus.setFill(wrongLightStatus);
+            }else if(LogicCalcu.isKontingensiStatus()){
+                tautologiLightStatus.setFill(wrongLightStatus);
+                kontradiksiLightStatus.setFill(wrongLightStatus);
+                kontingensiLightStatus.setFill(rightLightStatus);
+            }else{
+                tautologiLightStatus.setFill(wrongLightStatus);
+                kontradiksiLightStatus.setFill(wrongLightStatus);
+                kontingensiLightStatus.setFill(wrongLightStatus);
+            }
+
+        }
     }
+
+
+
 
 
 
